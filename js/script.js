4a1c1e65aa01cc0bd1c93ccbd256bbc3fdff989a
mairi tikk <mairi.tikk@gmail.com>
2024-01-31 13:58:25 +0100

    chansing nav bar

--- js/script.js
+++ js/script.js
@@ -11,3 +11,12 @@ function removeActiveClasses() {
 
     })
 }
+
+const search = document.querySelector('.search')
+const btn = document.querySelector('.btn')
+const input = document.querySelector('.input')
+
+btn.addEventListener('click', () => {
+    search.classList.toggle('active')
+    input.focus()
+})
