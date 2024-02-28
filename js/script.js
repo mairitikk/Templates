b08c4c3c2f270e572053cbd6b648aa9f9da828a0
mairi tikk <mairi.tikk@gmail.com>
2024-02-28 09:39:39 +0100

    nav bar changes

--- js/script.js
+++ js/script.js
@@ -18,7 +18,15 @@ btn.addEventListener('click', () => {
     search.classList.toggle('active')
     input.focus()
 })
-
+//navbar burger menu
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
 
 
 // days counter
