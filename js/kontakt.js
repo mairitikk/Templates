bc250519a62d37080469a8ded969761bbd9893fa
mairi tikk <mairi.tikk@gmail.com>
2024-03-22 15:08:24 +0100

    add some mobile first styls

--- js/kontakt.js
+++ js/kontakt.js
@@ -30,3 +30,13 @@ $('button').click(function () {
     $('.title').toggleClass('active');
     $('nav').toggleClass('active');
 });
+
+//navbar burger menu
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
